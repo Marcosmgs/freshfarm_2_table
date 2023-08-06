b71252306d4279b4f9d0640606f73fd5cc30797a
marcosmgs <marcosmgs2023@gmail.com>
2023-08-06 19:54:16 +0000

    created checkout view and template

--- ff_2_table/settings.py
+++ ff_2_table/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
-    'checkout',    
+    'checkout',
+
+    # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ff_2_table.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

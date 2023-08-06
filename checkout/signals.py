259bc9f86dca2de308352bfd0ef6861c844e37f3
marcosmgs <marcosmgs2023@gmail.com>
2023-08-06 17:28:16 +0000

    created checkout form, admin and signals

---
+++ checkout/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Recalculate order total when a line item is added or modified.
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Recalculate order total when a line item is removed.
+    """
+    instance.order.update_total()

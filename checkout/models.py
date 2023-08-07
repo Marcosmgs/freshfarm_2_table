5dd29f36c8b2a21bade059d07b53eecca7b809a9
marcosmgs <marcosmgs2023@gmail.com>
2023-08-07 12:35:34 +0000

    created view logic and template for checkou success

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Calculate the new total whenever a new item is added, 
         including the cost of delivery.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:

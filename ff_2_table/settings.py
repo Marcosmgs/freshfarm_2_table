d467b1738e7260fb6b4775c3d908d89e1ea260a1
marcosmgs <marcosmgs2023@gmail.com>
2023-08-10 15:00:06 +0000

    removed secret key and set debug

--- ff_2_table/settings.py
+++ ff_2_table/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x4k9ksf#*u6s9s-v+oh%qqdv@s4up-#oeft_t7vl6=!&)yle-*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fresh-farm-2-table-270bbb259c1e.herokuapp.com', 'localhost']
 

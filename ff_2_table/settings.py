1f70d94d678abc262557ba3542b57ff189c8aeb9
marcosmgs <marcosmgs2023@gmail.com>
2023-08-12 09:29:23 +0000

    login, registration and navigation refactoring

--- ff_2_table/settings.py
+++ ff_2_table/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'thisismysecretkeysupersecret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['fresh-farm-2-table-270bbb259c1e.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['fresh-farm-2-table-270bbb259c1e.herokuapp.com', '8000-marcosmgs-freshfarm2tab-rs2z33y0zg5.ws-eu103.gitpod.io']
 
 
 # Application definition

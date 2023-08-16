1b9bb97139947ecb6d82686eef8eb6e34e083c91
marcosmgs <marcosmgs2023@gmail.com>
2023-08-16 18:04:34 +0000

    added meta tags and turned debug to false

--- ff_2_table/settings.py
+++ ff_2_table/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'thisismysecretkeysupersecret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['fresh-farm-2-table-270bbb259c1e.herokuapp.com', '8000-marcosmgs-freshfarm2tab-rs2z33y0zg5.ws-eu103.gitpod.io']
 

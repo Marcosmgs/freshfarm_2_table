41421fd9e9cf6ff63cabf6db7304c9b04f713cb9
marcosmgs <marcosmgs2023@gmail.com>
2023-10-10 12:12:39 +0000

    created model feedback and added a button to access it

--- returnbox/models.py
+++ returnbox/models.py
@@ -19,3 +19,14 @@ class BoxReturn(models.Model):
 
     def __str__(self):
         return f"User: {self.user.user.username}, Status: {self.status}"
+
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
